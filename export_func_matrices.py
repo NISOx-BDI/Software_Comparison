762af9238b465fd3b020c94d2043ca6028dbb094
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-10-05 12:11:04 +0100

    write matrix

--- export_func_matrices.py
+++ export_func_matrices.py
@@ -1,6 +1,6 @@
 import glob
 import os
-import nibabel
+import nibabel as nib
 
 data_dir = "/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON\
 /ds001/FSL/LEVEL1/sub-**/run-**.feat/reg/"
@@ -9,3 +9,6 @@ data_dir = "/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON\
 reg_funcs = glob.glob(os.path.join(data_dir, 'example_func2highres.nii.gz'))
 for reg_func in reg_funcs:
     print(reg_func)
+    img = nib.load(reg_func)
+    print(img.affine)
+    print("-----")
