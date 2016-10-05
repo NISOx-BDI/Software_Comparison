f6a757089afa1a4f1bcff0e9cda2bcc22addc1f8
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-10-05 11:22:50 +0100

    filename: example_func2highres

--- export_func_matrices.py
+++ export_func_matrices.py
@@ -5,5 +5,5 @@ data_dir = "/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON\
 /ds001/FSL/LEVEL1/sub-01/run-01.feat/reg/"
 
 
-#example_func2highres.nii
-print(glob.glob(os.path.join(data_dir, '*.nii.gz')))
+#.nii
+print(glob.glob(os.path.join(data_dir, 'example_func2highres.nii.gz')))
