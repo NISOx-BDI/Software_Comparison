8643f51583cf08c46fc812fe2fc08fd7a6b1afa5
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-10-28 15:01:08 +0100

    Uncomment all

--- lib/run_group_level_analysis.m
+++ lib/run_group_level_analysis.m
@@ -3,12 +3,16 @@ function run_group_level_analysis(level1_dir, group_batch_template, level2_dir, 
 
     scripts_dir = fullfile(level1_dir, '..', 'SCRIPTS');
 
-    OUT_DIR = level2_dir
-
+    OUT_DIR = level2_dir;
+    
+    if ~isdir(scripts_dir)
+        mkdir(scripts_dir)
+    end
+    
     num_sub = numel(sub_dirs);
     CON_FILES = cell(num_sub,0);
     for i = 1:num_sub
-        CON_FILES{i} = spm_select(...
+        CON_FILES{i,1} = spm_select(...
             'FPList', sub_dirs{i}, ['con_' contrast_id '\.nii']);
     end
 
