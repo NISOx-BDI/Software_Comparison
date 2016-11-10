213a3ef151360114d67e64239855982b36d3b728
Alex Bowring <A.Bowring@warwick.ac.uk>
2016-11-10 14:57:00 +0000

    SPM script takes ignored volumes and TR times as variables

--- lib/run_subject_level_analyses.m
+++ lib/run_subject_level_analyses.m
@@ -1,4 +1,4 @@
-function run_subject_level_analyses(raw_dir, preproc_dir, sub_template, level1_dir)
+function run_subject_level_analyses(raw_dir, preproc_dir, sub_template, level1_dir, num_ignored_volumes)
     sub_dirs = cellstr(spm_select('FPList',raw_dir, 'dir','sub-*'));
 
     onset_dir = fullfile(preproc_dir, '..', 'ONSETS');
@@ -20,7 +20,8 @@ function run_subject_level_analyses(raw_dir, preproc_dir, sub_template, level1_d
         fmri_files = cellstr(spm_select('List', func_dir, [sub '.*\.nii']));
         for r = 1:numel(fmri_files)
             sub_run = [sub '.*_run-' sprintf('%02d',r)];
-            fmris = cellstr(spm_select('ExtFPList', func_dir, [sub_run '.*\.nii'], 3:300)); 
+            fmris = cellstr(spm_select('ExtFPList', func_dir, [sub_run '.*\.nii'], Inf)); 
+            fmris = fmris(num_ignored_volumes+1:end);
             eval(['FUNC_RUN_' num2str(r) ' =  fmris;']);
             onset_file = spm_select('FPList', onset_dir, [sub_run '.*\.mat']);
             eval(['ONSETS_RUN_' num2str(r) ' = onset_file;']);
--- process_ds001.m
+++ process_ds001.m
@@ -9,6 +9,12 @@ preproc_dir = fullfile(spm_dir, 'PREPROCESSING');
 level1_dir = fullfile(spm_dir, 'LEVEL1');
 level2_dir = fullfile(spm_dir, 'LEVEL2');
 
+num_ignored_volumes = 2;
+% Specify the number of functional volumes ignored in the study
+
+removed_TR_time = 4;
+% Specify the TR that will be removed from onsets, equal to num_ignored_volumes*TR
+
 % Add 'lib' folder to the matlab path
 if ~exist('copy_gunzip', 'file')
     addpath(fullfile(fileparts(mfilename('fullpath')), 'lib'))
@@ -33,8 +39,8 @@ CondNames = {...
     {{'control_pumps_fixed','control_pumps_demean'}, {'control_pumps_demean', 0, 'control_pumps_demean'}},...
     {'control_pumps_RT', {'control_pumps_demean', 'response_time'}}};
 
-create_onset_files(study_dir, onsetDir, CondNames);
+create_onset_files(study_dir, onsetDir, CondNames, removed_TR_time);
 spm('defaults','FMRI');
-run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir);
+run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir, num_ignored_volumes);
 
 run_group_level_analysis(level1_dir, 'template_ds001_SPM_level2', level2_dir, '0001');
