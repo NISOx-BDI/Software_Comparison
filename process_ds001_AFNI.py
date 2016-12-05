757aaabd343f033ea5e79bbbcab7c4ccd5344c45
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-12-05 10:32:48 +0000

    Uncomment everything

--- process_ds001_AFNI.py
+++ process_ds001_AFNI.py
@@ -26,7 +26,7 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 
 # Copy raw anatomical and functional data to the preprocessing directory and
 # run BET on the anatomical images
-#copy_raw(raw_dir, preproc_dir)
+copy_raw(raw_dir, preproc_dir)
 
 # Directory to store the onset files
 onset_dir = os.path.join(afni_dir, 'ONSETS')
@@ -46,14 +46,14 @@ conditions = (
     ('control_pumps_RT', ('control_pumps_demean', 'response_time')))
 
 # Create onset files based on BIDS tsv files
-#cond_files = create_afni_onset_files(raw_dir, onset_dir, conditions)
+cond_files = create_afni_onset_files(raw_dir, onset_dir, conditions)
 
 sub_level_template = os.path.join(cwd, 'lib', 'template_ds001_AFNI_level1')
 grp_level_template = os.path.join(cwd, 'lib', 'template_ds001_AFNI_level2')
 
 # Run a GLM combining all the fMRI runs of each subject
-#run_subject_level_analyses(preproc_dir, onset_dir, level1_dir,
-#    sub_level_template)
+run_subject_level_analyses(preproc_dir, onset_dir, level1_dir,
+   sub_level_template)
 
 # # Run the group-level GLM
 run_group_level_analysis(level1_dir, level2_dir, grp_level_template)
