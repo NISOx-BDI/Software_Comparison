677308ac0b8e18142cd3e327e4cd2109e56c440d
Alex Bowring <A.Bowring@warwick.ac.uk>
2017-02-20 12:09:12 +0000

    AFNI 1st level analyses finished

---
+++ sub-43_level1.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env tcsh
+
+# created by uber_subject.py: version 0.39 (March 21, 2016)
+# creation date: Tue Sep 13 15:59:22 2016
+
+# run afni_proc.py to create a single subject processing script
+afni_proc.py -subj_id sub43                                                  \
+        -script proc.sub43 -scr_overwrite                                    \
+        -blocks tshift align tlrc volreg blur mask scale regress               \
+        -copy_anat /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/PREPROCESSING/ANATOMICAL/sub-43_T1w.nii                                    \
+        -align_opts_aea -giant_move -check_flip                                \
+        -dsets                                                                 \
+            /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/PREPROCESSING/FUNCTIONAL/sub-43_task-theoryofmindwithmanualresponse_run-01_bold.nii.gz     \
+            /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/PREPROCESSING/FUNCTIONAL/sub-43_task-theoryofmindwithmanualresponse_run-02_bold.nii.gz     \
+        -volreg_align_to third                                                 \
+        -volreg_align_e2a                                                      \
+        -volreg_tlrc_warp                                                      \
+        -blur_size 8.0                                                         \
+        -regress_stim_times                                                    \
+            /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/ONSETS/sub-43_combined_false_belief_story_afni.1d               \
+            /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/ONSETS/sub-43_combined_false_belief_question_afni.1d            \
+            /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/ONSETS/sub-43_combined_false_photo_story_afni.1d                \
+            /Users/maullz/Desktop/Software_Comparison/ds109/AFNI/ONSETS/sub-43_combined_false_photo_question_afni.1d             \
+        -regress_stim_labels                                                   \
+            false_belief_story false_belief_question false_photo_story         \
+            false_photo_question                                               \
+        -regress_basis_multi                                                   \
+            'SPMG1' 'SPMG1' 'SPMG1' 'SPMG1'                                    \
+        -regress_censor_motion 0.3                                             \
+        -regress_opts_3dD                                                      \
+            -gltsym 'SYM: false_belief_story -false_photo_story'               \
+        -glt_label 1 false_belief_vs_false_photo                               \
+        -regress_make_ideal_sum sum_ideal.1D                                   \
+        -regress_est_blur_epits                                                \
+        -regress_est_blur_errts
+        
