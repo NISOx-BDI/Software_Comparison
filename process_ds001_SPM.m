24edaa3d15540021eccdfe229da25d1468245133
AlexBowring <A.bowring@warwick.ac.uk>
2017-04-11 11:43:27 +0100

    SPM ds001 permutation test files

--- process_ds001_SPM.m
+++ process_ds001_SPM.m
@@ -9,6 +9,7 @@ end
 preproc_dir = fullfile(spm_dir, 'PREPROCESSING');
 level1_dir = fullfile(spm_dir, 'LEVEL1');
 level2_dir = fullfile(spm_dir, 'LEVEL2');
+perm_dir = fullfile(level2_dir, 'permutation_test');
 
 % Specify the number of functional volumes ignored in the study
 TR = 2;
@@ -42,7 +43,8 @@ CondNames = {...
     {{'control_pumps_fixed','control_pumps_demean'}, {'control_pumps_demean', 0, 'control_pumps_demean'}},...
     {'control_pumps_RT', {'control_pumps_demean', 'response_time'}}};
 
-create_onset_files(study_dir, onsetDir, CondNames, removed_TR_time);
+% create_onset_files(study_dir, onsetDir, CondNames, removed_TR_time);
 spm('defaults','FMRI');
 run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir, num_ignored_volumes, TR);
 run_group_level_analysis(level1_dir, 'template_ds001_SPM_level2', level2_dir, '0001');
+run_permutation_test(level1_dir, 'template_ds001_SPM_perm_test', perm_dir, '0001');
