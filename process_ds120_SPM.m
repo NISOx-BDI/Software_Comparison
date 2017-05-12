527b79c929db633c504fbf990ba96139a9c2978d
Alex Bowring <A.Bowring@warwick.ac.uk>
2017-05-12 14:36:15 +0100

    making Amendds120tsv.sh run from main scripts

--- process_ds120_SPM.m
+++ process_ds120_SPM.m
@@ -1,9 +1,17 @@
-base_dir = '/home/maullz/NIDM-Ex/BIDS_Data';
+rbase_dir = '/home/maullz/NIDM-Ex/BIDS_Data';
 
 raw_dir = fullfile(base_dir, 'DATA', 'BIDS');
 results_dir = fullfile(base_dir, 'RESULTS', 'SOFTWARE_COMPARISON');
 
+pre_study_dir = fullfile('ds120_R1.0.0');
 study_dir = fullfile(raw_dir, 'ds120_R1.0.0_AMENDED');
+
+% The original event files are not compatible with Bidsto3col.sh, so we copy the raw data and amend the events
+if ~exist(study_dir)
+	copy(pre_study_dir, study_dir);
+	system(['Amendds120tsv.sh ' study_dir])
+end 
+
 spm_dir = fullfile(results_dir, 'ds120', 'SPM');
 preproc_dir = fullfile(spm_dir, 'PREPROCESSING');
 level1_dir = fullfile(spm_dir, 'LEVEL1');
