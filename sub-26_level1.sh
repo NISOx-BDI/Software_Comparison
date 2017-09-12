0dfd4909c806450155677e51d9f14ba7f7c4c98f
AlexBowring <A.bowring@warwick.ac.uk>
2017-09-12 15:52:58 +0100

    First pass of 2mm ds120 AFNI results

--- sub-26_level1.sh
+++ sub-26_level1.sh
@@ -7,22 +7,23 @@
 afni_proc.py -subj_id sub26                                                  \
         -script proc.sub26 -scr_overwrite                                    \
         -blocks tshift align tlrc volreg blur mask scale regress               \
-        -copy_anat /Users/maullz/Desktop/Software_Comparison/ds120/AFNI/PREPROCESSING/ANATOMICAL/sub-26_T1w.nii                                    \
+        -copy_anat /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds120/AFNI/PREPROCESSING/ANATOMICAL/sub-26_T1w.nii                                    \
         -tcat_remove_first_trs 4                                               \
         -align_opts_aea -giant_move -check_flip                                \
         -dsets                                                                 \
-            /Users/maullz/Desktop/Software_Comparison/ds120/AFNI/PREPROCESSING/FUNCTIONAL/sub-26_task-antisaccadetaskwithfixedorder_run-01_bold.nii.gz    \
-            /Users/maullz/Desktop/Software_Comparison/ds120/AFNI/PREPROCESSING/FUNCTIONAL/sub-26_task-antisaccadetaskwithfixedorder_run-02_bold.nii.gz    \
-            /Users/maullz/Desktop/Software_Comparison/ds120/AFNI/PREPROCESSING/FUNCTIONAL/sub-26_task-antisaccadetaskwithfixedorder_run-03_bold.nii.gz    \
+            /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds120/AFNI/PREPROCESSING/FUNCTIONAL/sub-26_task-antisaccadetaskwithfixedorder_run-01_bold.nii.gz    \
+            /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds120/AFNI/PREPROCESSING/FUNCTIONAL/sub-26_task-antisaccadetaskwithfixedorder_run-02_bold.nii.gz    \
+            /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds120/AFNI/PREPROCESSING/FUNCTIONAL/sub-26_task-antisaccadetaskwithfixedorder_run-03_bold.nii.gz    \
         -tshift_opts_ts -tpattern alt+z                                        \
+        -tlrc_base MNI_avg152T1+tlrc                                           \
+        -volreg_warp_dxyz 2                                                    \
         -volreg_align_to third                                                 \
         -volreg_align_e2a                                                      \
         -volreg_tlrc_warp                                                      \
-        -tlrc_base MNI_avg152T1+tlrc                                           \
         -blur_size 5.0                                                         \
         -regress_stim_times                                                    \
-            /Users/maullz/Desktop/Software_Comparison/ds120/AFNI/ONSETS/sub-26_combined_neutral_afni.1D                          \
-            /Users/maullz/Desktop/Software_Comparison/ds120/AFNI/ONSETS/sub-26_combined_reward_afni.1D                           \
+            /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds120/AFNI/ONSETS/sub-26_combined_neutral_afni.1d                          \
+            /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds120/AFNI/ONSETS/sub-26_combined_reward_afni.1d                           \
         -regress_stim_labels                                                   \
             neutral reward                                                     \
         -regress_basis_multi                                                   \
