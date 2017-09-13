787042e60e4467e3545a707b48db427e6c35ffed
Alex Bowring <A.Bowring@warwick.ac.uk>
2017-09-13 14:32:11 +0100

    ds120 notebook complete, alterred functions to work with ds120 and old studies

--- lib/dice.py
+++ lib/dice.py
@@ -23,10 +23,10 @@ def sorrenson_dice(data1, data2):
     dice_coefficient = numerator/denominator
     return dice_coefficient
 
-def dice(afni_exc_set_file, afni_exc_set_file_neg, 
-         spm_exc_set_file, spm_exc_set_file_neg,
-         afni_reslice_spm_pos_exc, afni_reslice_spm_neg_exc, 
-         afni_spm_reslice_pos_exc, afni_spm_reslice_neg_exc,
+def dice(afni_exc_set_file, spm_exc_set_file,
+         afni_reslice_spm_pos_exc, afni_spm_reslice_pos_exc, 
+         afni_exc_set_file_neg=None, spm_exc_set_file_neg=None,
+         afni_reslice_spm_neg_exc=None, afni_spm_reslice_neg_exc=None,
          afni_reslice_fsl_pos_exc=None, afni_reslice_fsl_neg_exc=None, 
          afni_fsl_reslice_pos_exc=None, afni_fsl_reslice_neg_exc=None,
          fsl_exc_set_file=None, fsl_exc_set_file_neg=None, 
@@ -35,12 +35,14 @@ def dice(afni_exc_set_file, afni_exc_set_file_neg,
 
     # Get data from excursion set images
     afni_pos_dat = nib.load(afni_exc_set_file).get_data()
-    afni_neg_dat = nib.load(afni_exc_set_file_neg).get_data()
+    if afni_exc_set_file_neg is not None:
+        afni_neg_dat = nib.load(afni_exc_set_file_neg).get_data()
     if fsl_exc_set_file is not None:
         fsl_pos_dat = nib.load(fsl_exc_set_file).get_data()
         fsl_neg_dat = nib.load(fsl_exc_set_file_neg).get_data()
     spm_pos_dat = nib.load(spm_exc_set_file).get_data()
-    spm_neg_dat = nib.load(spm_exc_set_file_neg).get_data()
+    if spm_exc_set_file_neg is not None:
+        spm_neg_dat = nib.load(spm_exc_set_file_neg).get_data()
 
     # Get data from resliced images
     if fsl_exc_set_file is not None:
@@ -50,9 +52,11 @@ def dice(afni_exc_set_file, afni_exc_set_file_neg,
         afni_fsl_res_neg_dat = nib.load(afni_fsl_reslice_neg_exc).get_data()
 
     afni_res_spm_pos_dat = nib.load(afni_reslice_spm_pos_exc).get_data()
-    afni_res_spm_neg_dat = nib.load(afni_reslice_spm_neg_exc).get_data()
+    if afni_reslice_spm_neg_exc is not None:
+        afni_res_spm_neg_dat = nib.load(afni_reslice_spm_neg_exc).get_data()
     afni_spm_res_pos_dat = nib.load(afni_spm_reslice_pos_exc).get_data()
-    afni_spm_res_neg_dat = nib.load(afni_spm_reslice_neg_exc).get_data()
+    if afni_spm_reslice_neg_exc is not None:
+        afni_spm_res_neg_dat = nib.load(afni_spm_reslice_neg_exc).get_data()
     
     if fsl_exc_set_file is not None:
         fsl_res_spm_pos_dat = nib.load(fsl_reslice_spm_pos_exc).get_data()
@@ -67,8 +71,10 @@ def dice(afni_exc_set_file, afni_exc_set_file_neg,
 
     afni_res_spm_pos_dice = sorrenson_dice(afni_res_spm_pos_dat, spm_pos_dat)
     afni_spm_res_pos_dice = sorrenson_dice(afni_spm_res_pos_dat, afni_pos_dat)
-    afni_res_spm_neg_dice = sorrenson_dice(afni_res_spm_neg_dat, spm_neg_dat)
-    afni_spm_res_neg_dice = sorrenson_dice(afni_spm_res_neg_dat, afni_neg_dat)
+    if afni_reslice_spm_neg_exc is not None:
+        afni_res_spm_neg_dice = sorrenson_dice(afni_res_spm_neg_dat, spm_neg_dat)
+    if afni_spm_reslice_neg_exc is not None:
+        afni_spm_res_neg_dice = sorrenson_dice(afni_spm_res_neg_dat, afni_neg_dat)
     
     if fsl_exc_set_file is not None:
         fsl_res_spm_pos_dice = sorrenson_dice(fsl_res_spm_pos_dat, spm_pos_dat)
@@ -85,8 +91,9 @@ def dice(afni_exc_set_file, afni_exc_set_file_neg,
     print "AFNI/SPM positive activation dice coefficient = %s" % afni_res_spm_pos_dice
     print "SPM/AFNI positive activation dice coefficient = %s\n" % afni_spm_res_pos_dice
 
-    print "AFNI/SPM negative activation dice coefficient = %s" % afni_res_spm_neg_dice
-    print "SPM/AFNI negative activation dice coefficient = %s\n" % afni_spm_res_neg_dice
+    if afni_reslice_spm_neg_exc is not None:
+        print "AFNI/SPM negative activation dice coefficient = %s" % afni_res_spm_neg_dice
+        print "SPM/AFNI negative activation dice coefficient = %s\n" % afni_spm_res_neg_dice
 
     if fsl_exc_set_file is not None:
         print "FSL/SPM positive activation dice coefficient = %s" % fsl_res_spm_pos_dice
--- lib/plot_excursion_sets.py
+++ lib/plot_excursion_sets.py
@@ -7,27 +7,41 @@ from nilearn._utils.niimg_conversions import _safe_get_data
 import nibabel as nib
 
 def plot_excursion_sets(exc_sets):
+    if len(sorted(exc_sets.items())[0][1][1]) == 2:
+        for soft, (mask_file, (exc_set_file, exc_set_file_neg), stat_file) in sorted(exc_sets.items()):
+            # Remove NaNs
+            n = nib.load(exc_set_file)
+            d = n.get_data()
+            exc_set_nonan = nib.Nifti1Image(np.nan_to_num(d), n.affine, header=n.header)
 
-    for soft, (mask_file, (exc_set_file, exc_set_file_neg), stat_file) in sorted(exc_sets.items()):
-        # Remove NaNs
-        n = nib.load(exc_set_file)
-        d = n.get_data()
-        exc_set_nonan = nib.Nifti1Image(np.nan_to_num(d), n.affine, header=n.header)
+            n = nib.load(exc_set_file_neg)
+            d = n.get_data()
+            exc_set_neg_nonan = nib.Nifti1Image(np.nan_to_num(d), n.affine, header=n.header)
 
-        n = nib.load(exc_set_file_neg)
-        d = n.get_data()
-        exc_set_neg_nonan = nib.Nifti1Image(np.nan_to_num(d), n.affine, header=n.header)
+            # Combine activations and deactivations in a single image 
+            to_display = math_img("img1-img2", img1=exc_set_nonan, img2=exc_set_neg_nonan)
 
-        # Combine activations and deactivations in a single image 
-        to_display = math_img("img1-img2", img1=exc_set_nonan, img2=exc_set_neg_nonan)
+            # Display statistic maps
+            display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=[4, 32], draw_cross=False, colorbar=True, title=soft.upper(), threshold=0.000001, vmax=4.2)
+            display = plotting.plot_stat_map(to_display, cut_coords=[38], draw_cross=False, display_mode='y', threshold=0.000001, colorbar=False, vmax=4.2)
+            # Additional plot: slices along z
+            display = plotting.plot_stat_map(to_display, cut_coords=[-32, -18, 0, 12, 24, 40, 58], draw_cross=False, display_mode='z', threshold=0.000001, colorbar=False, vmax=4.2, title=soft.upper())
 
-        # Display statistic maps
-        display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=[4, 32], draw_cross=False, colorbar=True, 
-                               title=soft.upper(), threshold=0.000001, vmax=4.2)
-        display = plotting.plot_stat_map(to_display, cut_coords=[38], draw_cross=False, display_mode='y', 
-                               threshold=0.000001, colorbar=False, vmax=4.2)
-        # Additional plot: slices along z
-        display = plotting.plot_stat_map(to_display, cut_coords=[-32, -18, 0, 12, 24, 40, 58], draw_cross=False, display_mode='z', 
-                               threshold=0.000001, colorbar=False, vmax=4.2, title=soft.upper())
+        plotting.show()
+    else:
+        for soft, (mask_file, exc_set_file, stat_file) in sorted(exc_sets.items()):
+            # Remove NaNs
+            n = nib.load(exc_set_file)
+            d = n.get_data()
+            exc_set_nonan = nib.Nifti1Image(np.nan_to_num(d), n.affine, header=n.header)
 
-    plotting.show()
+            # Combine activations and deactivations in a single image 
+            to_display = math_img("img1", img1=exc_set_nonan)
+
+            # Display statistic maps
+            display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=[4, 32], draw_cross=False, colorbar=True, title=soft.upper(), threshold=0.000001, vmax=4.2)
+            display = plotting.plot_stat_map(to_display, cut_coords=[38], draw_cross=False, display_mode='y', threshold=0.000001, colorbar=False, vmax=4.2)
+            # Additional plot: slices along z
+            display = plotting.plot_stat_map(to_display, cut_coords=[-32, -18, 0, 12, 24, 40, 58], draw_cross=False, display_mode='z', threshold=0.000001, colorbar=False, vmax=4.2, title=soft.upper())
+
+        plotting.show()
