70bc289c520e79356039abe622cfb3a3b2d5d927
Alex Bowring <A.Bowring@warwick.ac.uk>
2017-09-14 12:06:03 +0100

    plot_excursion_sets now take axial slices to display as an argument, notebooks updated

--- lib/plot_excursion_sets.py
+++ lib/plot_excursion_sets.py
@@ -6,7 +6,7 @@ from nilearn.image import load_img, new_img_like
 from nilearn._utils.niimg_conversions import _safe_get_data
 import nibabel as nib
 
-def plot_excursion_sets(exc_sets, max_activation):
+def plot_excursion_sets(exc_sets, max_activation, x_coords, y_coords, z_coords):
     if len(sorted(exc_sets.items())[0][1][1]) == 2:
         for soft, (mask_file, (exc_set_file, exc_set_file_neg), stat_file) in sorted(exc_sets.items()):
             # Remove NaNs
@@ -22,10 +22,10 @@ def plot_excursion_sets(exc_sets, max_activation):
             to_display = math_img("img1-img2", img1=exc_set_nonan, img2=exc_set_neg_nonan)
 
             # Display statistic maps
-            display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=[4, 32], draw_cross=False, colorbar=True, title=soft.upper(), threshold=0.000001, vmax=max_activation)
-            display = plotting.plot_stat_map(to_display, cut_coords=[38], draw_cross=False, display_mode='y', threshold=0.000001, colorbar=False, vmax=max_activation)
+            display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=x_coords, draw_cross=False, colorbar=True, title=soft.upper(), threshold=0.000001, vmax=max_activation)
+            display = plotting.plot_stat_map(to_display, cut_coords=y_coords, draw_cross=False, display_mode='y', threshold=0.000001, colorbar=False, vmax=max_activation)
             # Additional plot: slices along z
-            display = plotting.plot_stat_map(to_display, cut_coords=[-32, -18, 0, 12, 24, 40, 58], draw_cross=False, display_mode='z', threshold=0.000001, colorbar=False, vmax=max_activation, title=soft.upper())
+            display = plotting.plot_stat_map(to_display, cut_coords=z_coords, draw_cross=False, display_mode='z', threshold=0.000001, colorbar=False, vmax=max_activation, title=soft.upper())
 
         plotting.show()
     else:
@@ -39,9 +39,9 @@ def plot_excursion_sets(exc_sets, max_activation):
             to_display = math_img("img1", img1=exc_set_nonan)
 
             # Display statistic maps
-            display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=[4, 32], draw_cross=False, colorbar=True, title=soft.upper(), threshold=0.000001, vmax=max_activation)
-            display = plotting.plot_stat_map(to_display, cut_coords=[38], draw_cross=False, display_mode='y', threshold=0.000001, colorbar=False, vmax=max_activation)
+            display = plotting.plot_stat_map(to_display, display_mode='x', cut_coords=x_coords, draw_cross=False, colorbar=True, title=soft.upper(), threshold=0.000001, vmax=max_activation)
+            display = plotting.plot_stat_map(to_display, cut_coords=y_coords, draw_cross=False, display_mode='y', threshold=0.000001, colorbar=False, vmax=max_activation)
             # Additional plot: slices along z
-            display = plotting.plot_stat_map(to_display, cut_coords=[-32, -18, 0, 12, 24, 40, 58], draw_cross=False, display_mode='z', threshold=0.000001, colorbar=False, vmax=max_activation, title=soft.upper())
+            display = plotting.plot_stat_map(to_display, cut_coords=z_coords, draw_cross=False, display_mode='z', threshold=0.000001, colorbar=False, vmax=max_activation, title=soft.upper())
 
         plotting.show()
