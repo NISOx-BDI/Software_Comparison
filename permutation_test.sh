26305ac8018d01478953d20e8a71d4d43b322b36
AlexBowring <A.bowring@warwick.ac.uk>
2017-09-26 11:55:13 +0100

    AFNI ds001 and ds109 perm test files now appended with perm name

--- permutation_test.sh
+++ permutation_test.sh
@@ -1,7 +1,7 @@
 cd /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/permutation_test
 
 # t-test analysis
-3dttest++ -Clustsim 1 -prefix ttest++_Clustsim_result       \
+3dttest++ -Clustsim 1 -prefix perm_ttest++_Clustsim_result       \
           -mask /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/permutation_test/../mask.nii   \
           -setA setA                   \
              01 "/home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL1/sub-01/sub01.results/stats.sub01+tlrc.HEAD[31]" \
@@ -22,34 +22,34 @@ cd /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/
              16 "/home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL1/sub-16/sub16.results/stats.sub16+tlrc.HEAD[31]" 
 
 # Create a group mask
-3dMean -mask_inter -prefix mask `ls /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL1/sub-*/sub*.results/mask_group*.HEAD`
+3dMean -mask_inter -prefix perm_mask `ls /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL1/sub-*/sub*.results/mask_group*.HEAD`
 
-3dclust -1Dformat -nosum -1dindex 1 -1tindex 1 -2thresh -1e+09 2.326 -inmask -savemask Positive_clust_mask -dxyz=1 1.01 1117 /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/permutation_test/ttest++_Clustsim_result+tlrc.HEAD
+3dclust -1Dformat -nosum -1dindex 1 -1tindex 1 -2thresh -1e+09 2.326 -inmask -savemask perm_Positive_clust_mask -dxyz=1 1.01 1117 /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/permutation_test/perm_ttest++_Clustsim_result+tlrc.HEAD
 
 
-3dclust -1Dformat -nosum -1dindex 1 -1tindex 1 -2thresh -2.326 1e+09 -inmask -savemask Positive_clust_mask -dxyz=1 1.01 1117 /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/permutation_test/ttest++_Clustsim_result+tlrc.HEAD
+3dclust -1Dformat -nosum -1dindex 1 -1tindex 1 -2thresh -2.326 1e+09 -inmask -savemask perm_Positive_clust_mask -dxyz=1 1.01 1117 /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001/AFNI/LEVEL2/permutation_test/perm_ttest++_Clustsim_result+tlrc.HEAD
 
 # Masking t_stat 
-   3dcalc -a 'ttest++_Clustsim_result+tlrc[1]' -b 'mask+tlrc' \
-            -expr 'a*b' -prefix ttest++_Clustsim_result_t_stat_masked -datum float   
+   3dcalc -a 'perm_ttest++_Clustsim_result+tlrc[1]' -b 'perm_mask+tlrc' \
+            -expr 'a*b' -prefix perm_ttest++_Clustsim_result_t_stat_masked -datum float   
                          
 # Binarizing both cluster masks
-   3dcalc -a 'Positive_clust_mask+tlrc' -expr 'ispositive(a-0.5)' \
-            -prefix Positive_binary_clust_mask                 
+   3dcalc -a 'perm_Positive_clust_mask+tlrc' -expr 'ispositive(a-0.5)' \
+            -prefix perm_Positive_binary_clust_mask                 
 
-   3dcalc -a 'Negative_clust_mask+tlrc' -expr 'ispositive(a-0.5)' \
-            -prefix Negative_binary_clust_mask                
+   3dcalc -a 'perm_Negative_clust_mask+tlrc' -expr 'ispositive(a-0.5)' \
+            -prefix perm_Negative_binary_clust_mask                
 
-   3dcalc -a Positive_binary_clust_mask+tlrc -b 3ttest++_Clustsim_result_t_stat_masked+tlrc \
-            -expr 'a*b' -prefix ttest++_Clustsim_result_positive_t_stat_clustered -datum float   
+   3dcalc -a perm_Positive_binary_clust_mask+tlrc -b perm_3ttest++_Clustsim_result_t_stat_masked+tlrc \
+            -expr 'a*b' -prefix perm_ttest++_Clustsim_result_positive_t_stat_clustered -datum float   
 
-   3dcalc -a Negative_binary_clust_mask+tlrc -b ttest++_Clustsim_result_t_stat_masked+tlrc \
-            -expr '-a*b' -prefix ttest++_Clustsim_result_negative_t_stat_clustered -datum float
+   3dcalc -a perm_Negative_binary_clust_mask+tlrc -b perm_ttest++_Clustsim_result_t_stat_masked+tlrc \
+            -expr '-a*b' -prefix perm_ttest++_Clustsim_result_negative_t_stat_clustered -datum float
 
 # Convert to NIFTI for upload to Neurovault
-  3dAFNItoNIFTI -prefix ttest++_Clustsim_result_t_stat ttest++_Clustsim_result+tlrc"[1]"
-  3dAFNItoNIFTI ttest++_Clustsim_result_t_stat_masked+tlrc
-  3dAFNItoNIFTI -prefix Positive_clustered_t_stat ttest++_Clustsim_result_positive_t_stat_clustered+tlrc
-  3dAFNItoNIFTI -prefix Negative_clustered_t_stat ttest++_Clustsim_result_negative_t_stat_clustered+tlrc
-  3dAFNItoNIFTI mask+tlrc
+  3dAFNItoNIFTI -prefix perm_ttest++_Clustsim_result_t_stat perm_ttest++_Clustsim_result+tlrc"[1]"
+  3dAFNItoNIFTI perm_ttest++_Clustsim_result_t_stat_masked+tlrc
+  3dAFNItoNIFTI -prefix perm_Positive_clustered_t_stat perm_ttest++_Clustsim_result_positive_t_stat_clustered+tlrc
+  3dAFNItoNIFTI -prefix perm_Negative_clustered_t_stat perm_ttest++_Clustsim_result_negative_t_stat_clustered+tlrc
+  3dAFNItoNIFTI perm_mask+tlrc
 
