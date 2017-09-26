c640a42291cd2709d2ece7ab5a947ce855264200
AlexBowring <A.bowring@warwick.ac.uk>
2017-09-26 14:45:34 +0100

    Corrected FSL permutation test templates and ran

--- permutation_test.sh
+++ permutation_test.sh
@@ -2,4 +2,4 @@ cd /storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds109/FSL/
 
 fslmerge -t contrasts /storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds109/FSL/LEVEL1/sub-*/combined.gfeat/cope1.feat/stats/cope1.nii.gz
 randomise -i contrasts -o OneSampT -1 -x -c `ptoz 0.005` -n 10000 
-fslmaths OneSampT_clustere_corrp_tstat1 -thr 0.95 -bin -mul OneSampT_clustere_corrp_tstat1 05FWECorrected_OneSampT_clustere_corrp_tstat1
+fslmaths OneSampT_clustere_corrp_tstat1 -thr 0.95 -bin -mul OneSampT_tstat1 05FWECorrected_OneSampT_clustere_corrp_tstat1
