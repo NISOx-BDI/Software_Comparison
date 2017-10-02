9a8c39841917a51b4c21ffd5130b2a358a16492b
Alex Bowring <A.Bowring@warwick.ac.uk>
2017-10-02 13:33:02 +0100

    ds109 reslice images now reslices permutation tests

--- ds109_reslice_images.m
+++ ds109_reslice_images.m
@@ -8,10 +8,17 @@ if ~isdir(reslice_dir)
     mkdir(reslice_dir) 
 end
 
+%% AFNI t-statistic and excursion set files
 afni_stat_file = fullfile(study_dir, 'AFNI', 'LEVEL2', '3dMEMA_result_t_stat_masked.nii');
 afni_pos_exc_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'Positive_clustered_t_stat.nii');
 afni_neg_exc_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'Negative_clustered_t_stat.nii');
 
+%% AFNI permutation test files
+afni_perm_stat_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'permutation_test', 'perm_ttest++_Clustsim_result_t_stat_masked.nii');
+afni_perm_pos_exc_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'permutation_test', 'perm_Positive_clustered_t_stat.nii');
+
+
+%% FSL t-statistic and excursion set files
 gunzip(fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.feat', 'stats', 'tstat1.nii.gz'));
 fsl_stat_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.feat', 'stats', 'tstat1.nii');
 gunzip(fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.feat', 'thresh_zstat1.nii.gz'));
@@ -19,16 +26,37 @@ fsl_pos_exc_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.fe
 gunzip(fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.feat', 'thresh_zstat2.nii.gz'));
 fsl_neg_exc_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.feat', 'thresh_zstat2.nii');
 
+%% FSL permutation test files
+gunzip(fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', 'OneSampT_pos_tstat1.nii.gz'));
+fsl_perm_stat_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', 'OneSampT_pos_tstat1.nii.gz');
+gunzip(fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', '05FWECorrected_OneSampT_pos_exc_set.nii.gz'));
+fsl_perm_pos_exc_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', '05FWECorrected_OneSampT_pos_exc_set.nii.gz');
+gunzip(fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', '05FWECorrected_OneSampT_neg_exc_set.nii.gz'));
+fsl_perm_pos_exc_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', '05FWECorrected_OneSampT_neg_exc_set.nii.gz');
+
+
+%% SPM t-statistic and excursion set files
 spm_stat_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'spmT_0001.nii');
 spm_pos_exc_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'spmT_0001_thresholded.nii');
 spm_neg_exc_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'spmT_0002_thresholded.nii');
 
+%% SPM permutation test files
+spm_perm_stat_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'permutation_test', 'snpmT+.img');
+spm_perm_pos_exc_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'permutation_test', 'SnPM_pos_filtered.nii');
+
+
+%% t-statistics directories
 [afni_stat_dir, ~, ~] = fileparts(afni_stat_file);
 [fsl_stat_dir, ~, ~] = fileparts(fsl_stat_file);
 [fsl_exc_set_dir, ~, ~] = fileparts(fsl_pos_exc_file);
 [spm_stat_dir, ~, ~] = fileparts(spm_stat_file);
+[afni_perm_stat_dir, ~, ~] = fileparts(afni_perm_stat_file);
+[fsl_perm_stat_dir, ~, ~] = fileparts(fsl_perm_stat_file);
+[spm_perm_stat_dir, ~, ~] = fileparts(spm_perm_stat_file);
 
 
+%% Reslicing t-statistic and excursion set images
+% AFNI/FSL reslicing
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
 spm_reslice({afni_stat_file, fsl_stat_file}, reslice_flags);
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
@@ -44,7 +72,19 @@ spm_reslice({afni_neg_exc_file, fsl_neg_exc_file}, reslice_flags);
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
 spm_reslice({fsl_neg_exc_file, afni_neg_exc_file}, reslice_flags);
 
+%% AFNI/FSL permutations
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({afni_perm_stat_file, fsl_perm_stat_file}, reslice_flags);
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({fsl_perm_stat_file, afni_perm_stat_file}, reslice_flags);
 
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({afni_perm_pos_exc_file_file, fsl_perm_pos_exc_file}, reslice_flags);
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({fsl_perm_pos_exc_file_file, afni_perm_pos_exc_file}, reslice_flags);
+
+
+% AFNI/SPM reslicing
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_spm_');
 spm_reslice({afni_stat_file, spm_stat_file}, reslice_flags);
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_spm_');
@@ -60,7 +100,19 @@ spm_reslice({afni_neg_exc_file, spm_neg_exc_file}, reslice_flags);
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_spm_');
 spm_reslice({spm_neg_exc_file, afni_neg_exc_file}, reslice_flags);
 
+%% AFNI/SPM permutations
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({afni_perm_stat_file, spm_perm_stat_file}, reslice_flags);
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({spm_perm_stat_file, afni_perm_stat_file}, reslice_flags);
 
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({afni_perm_pos_exc_file_file, spm_perm_pos_exc_file}, reslice_flags);
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({spm_perm_pos_exc_file_file, afni_perm_pos_exc_file}, reslice_flags);
+
+
+% FSL/SPM reslicing
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'fsl_spm_');
 spm_reslice({fsl_stat_file, spm_stat_file}, reslice_flags);
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'fsl_spm_');
@@ -75,6 +127,18 @@ reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wr
 spm_reslice({fsl_neg_exc_file, spm_neg_exc_file}, reslice_flags);
 reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'fsl_spm_');
 spm_reslice({spm_neg_exc_file, fsl_neg_exc_file}, reslice_flags);
+
+%% FSL/SPM permutations
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({fsl_perm_stat_file, spm_perm_stat_file}, reslice_flags);
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({spm_perm_stat_file, fsl_perm_stat_file}, reslice_flags);
+
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({fsl_perm_pos_exc_file_file, spm_perm_pos_exc_file}, reslice_flags);
+reslice_flags = struct('mask', true, 'mean', false, 'interp', 0, 'which', 1, 'wrap', [1 1 0], 'prefix', 'afni_fsl_');
+spm_reslice({spm_perm_pos_exc_file_file, fsl_perm_pos_exc_file}, reslice_flags);
+
 
 
 % Files where FSL has been resliced onto AFNI
