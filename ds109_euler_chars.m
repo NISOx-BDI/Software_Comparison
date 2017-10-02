15d00867ed0fbb01d836d2dab7d01f0f8e25dbc8
Alex Bowring <A.Bowring@warwick.ac.uk>
2017-10-02 17:01:09 +0100

    started to work permutations into notebooks and modified euler chars for permutations

--- ds109_euler_chars.m
+++ ds109_euler_chars.m
@@ -5,7 +5,11 @@ study_dir = fullfile(base_dir, study);
 spm_stat_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'spmT_0001.nii');
 fsl_stat_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'cope1.feat', 'stats', 'tstat1.nii.gz');
 afni_stat_file = fullfile(study_dir, 'AFNI', 'LEVEL2', '3dMEMA_result_t_stat_masked.nii');
-stat_file_array = {spm_stat_file, fsl_stat_file, afni_stat_file};
+spm_perm_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'permutation_test', 'snpmT+.img');
+fsl_perm_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', 'OneSampT_pos_tstat1.nii');
+afni_perm_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'permutation_test', 'perm_ttest++_Clustsim_result_t_stat_masked.nii');
+euler_array = {spm_stat_file, fsl_stat_file, afni_stat_file, spm_perm_file, fsl_perm_file, afni_perm_file};
+
 
 for i=1:length(stat_file_array)
 	euler_chars(stat_file_array{i});
