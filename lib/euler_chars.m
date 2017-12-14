e90dbb46d8087d5ca0c9135b2ce602288c75b65c
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2017-12-14 19:20:20 +0000

    euler chars now uses masks to pad with NaNs

--- lib/euler_chars.m
+++ lib/euler_chars.m
@@ -1,9 +1,9 @@
-function euler_chars(Statistic_Map)
+function euler_chars(Statistic_Map, Mask)
 	V = spm_vol(Statistic_Map);
 	X = spm_read_vols(V);
-	X(X==0) = NaN; 
-	X(X==100) = NaN;
-	X(X==-100) = NaN;
+	Mask = spm_vol(Mask);
+	M = spm_read_vols(M);
+	X(M==0) = NaN;
 	T = -6:0.1:6;
 	EC = zeros(size(T));
 	Binout = V(1); 
