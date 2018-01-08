681ce4dc75e3ff581bc088e78a76d45227955f42
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2018-01-08 14:23:49 +0000

    euler chars scripts bug fixed using masks

--- ds001_euler_chars.m
+++ ds001_euler_chars.m
@@ -12,9 +12,14 @@ afni_stat_file = fullfile(study_dir, 'AFNI', 'LEVEL2', '3dMEMA_result_t_stat_mas
 spm_perm_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'permutation_test', 'snpmT+.img');
 fsl_perm_file = fullfile(study_dir, 'FSL', 'LEVEL2', 'permutation_test', 'OneSampT_tstat1.nii.gz');
 afni_perm_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'permutation_test', 'perm_ttest++_Clustsim_result_t_stat_masked.nii');
+spm_mask = fullfile(study_dir, 'SPM', 'LEVEL2', 'mask.nii');
+fsl_mask = fullfile(study_dir, 'FSL', 'LEVEL2', 'group.gfeat', 'mask.nii.gz');
+afni_mask = fullfile(study_dir, 'AFNI', 'LEVEL2', 'mask.nii');
+
 euler_array = {spm_stat_file, fsl_stat_file, afni_stat_file, spm_perm_file, fsl_perm_file, afni_perm_file};
+mask_array = {spm_mask, fsl_mask, afni_mask, spm_mask, fsl_mask, afni_mask};
 
 
 for i=1:length(euler_array)
-	euler_chars(euler_array{i});
+	euler_chars(euler_array{i}, mask_array{i});
 end
--- ds120_euler_chars.m
+++ ds120_euler_chars.m
@@ -8,9 +8,13 @@ end
 study_dir = fullfile(base_dir, study);
 spm_stat_file = fullfile(study_dir, 'SPM', 'LEVEL2', 'spmF_0002.nii');
 afni_stat_file = fullfile(study_dir, 'AFNI', 'LEVEL2', 'Group_f_stat_masked.nii');
+spm_mask = fullfile(study_dir, 'SPM', 'LEVEL2', 'mask.nii');
+afni_mask = fullfile(study_dir, 'AFNI', 'LEVEL2', 'mask.nii');
+
 euler_array = {spm_stat_file, afni_stat_file};
+mask_array = {spm_mask, afni_mask};
 
 
 for i=1:length(euler_array)
-	euler_chars_f_statistic(euler_array{i});
+	euler_chars_f_statistic(euler_array{i}, mask_array{i});
 end
