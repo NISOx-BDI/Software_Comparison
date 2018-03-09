adcd666eb098164372533e776c1c959d2dbc19ef
cmaumet <camille.maumet@inria.fr>
2018-03-09 15:33:48 +0100

    Reslice image not needed anymore

--- lib/download_data.py
+++ lib/download_data.py
@@ -88,68 +88,6 @@ def download_data(nv_collection, study):
             (('Group_f_stat_masked.nii.gz', 'afni_stat.nii.gz'),)
             )
 
-    resl_images = (
-        'afni_spm_reslice.nii.gz',
-        'afni_reslice_spm.nii.gz',
-        )
-
-    if study not in ('ds109', 'ds120'):
-        # There is no deactivations in ds109 with AFNI perm and SnPM
-        # There is no permutation analysis for ds120
-        resl_images = resl_images + (
-            'afni_fsl_reslice_neg_exc_perm.nii.gz',
-            'afni_reslice_fsl_neg_exc_perm.nii.gz',
-            'afni_spm_reslice_neg_exc_perm.nii.gz',
-            'afni_reslice_spm_neg_exc_perm.nii.gz',
-            'fsl_spm_reslice_neg_exc_perm.nii.gz',
-            'fsl_reslice_spm_neg_exc_perm.nii.gz',
-            'afni_perm_reslice_fsl_neg_exc.nii.gz',
-            'afni_perm_fsl_reslice_neg_exc.nii.gz',
-            'afni_perm_reslice_spm_neg_exc.nii.gz',
-            'afni_perm_spm_reslice_neg_exc.nii.gz',
-            'afni_reslice_spm_perm_neg_exc.nii.gz',
-            'afni_spm_perm_reslice_neg_exc.nii.gz',
-            'fsl_reslice_spm_perm_neg_exc.nii.gz',
-            'fsl_spm_perm_reslice_neg_exc.nii.gz'
-            )
-
-    if study not in ('ds120'):
-        # There was no permutation analysis for ds120
-        resl_images = (
-            resl_images +
-            # There is only one excursion set for ds120 (no separate + and -)
-            ('afni_spm_reslice_pos_exc.nii.gz','afni_spm_reslice_neg_exc.nii.gz',
-             'afni_reslice_spm_pos_exc.nii.gz','afni_reslice_spm_neg_exc.nii.gz') + 
-            # There is no FSL analysis for ds120
-            ('afni_fsl_reslice_perm.nii.gz', 'afni_fsl_reslice_pos_exc_perm.nii.gz',
-             'afni_reslice_fsl_perm.nii.gz', 'afni_reslice_fsl_pos_exc_perm.nii.gz',
-             'afni_spm_reslice_perm.nii.gz', 'afni_spm_reslice_pos_exc_perm.nii.gz',
-             'afni_reslice_spm_perm.nii.gz', 'afni_reslice_spm_pos_exc_perm.nii.gz',
-             'fsl_spm_reslice_perm.nii.gz', 'fsl_spm_reslice_pos_exc_perm.nii.gz',
-             'fsl_reslice_spm_perm.nii.gz', 'fsl_reslice_spm_pos_exc_perm.nii.gz',
-             'afni_perm_reslice_fsl_pos_exc.nii.gz',
-             'afni_perm_fsl_reslice_pos_exc.nii.gz',
-             'afni_reslice_fsl_perm_pos_exc.nii.gz', 'afni_reslice_fsl_perm_neg_exc.nii.gz',
-             'afni_fsl_perm_reslice_pos_exc.nii.gz', 'afni_fsl_perm_reslice_neg_exc.nii.gz',                        
-             'afni_perm_reslice_spm_pos_exc.nii.gz',
-             'afni_perm_spm_reslice_pos_exc.nii.gz',
-             'afni_reslice_spm_perm_pos_exc.nii.gz',
-             'afni_spm_perm_reslice_pos_exc.nii.gz',
-             'fsl_perm_reslice_spm_pos_exc.nii.gz', 'fsl_perm_reslice_spm_neg_exc.nii.gz',
-             'fsl_perm_spm_reslice_pos_exc.nii.gz', 'fsl_perm_spm_reslice_neg_exc.nii.gz',        
-             'fsl_reslice_spm_perm_pos_exc.nii.gz',
-             'fsl_spm_perm_reslice_pos_exc.nii.gz') +
-            # Thre is no FSL analysis for ds120
-            ('afni_fsl_reslice.nii.gz', 'afni_fsl_reslice_pos_exc.nii.gz','afni_fsl_reslice_neg_exc.nii.gz',
-             'afni_reslice_fsl.nii.gz', 'afni_reslice_fsl_pos_exc.nii.gz','afni_reslice_fsl_neg_exc.nii.gz',
-             'fsl_spm_reslice.nii.gz', 'fsl_spm_reslice_pos_exc.nii.gz','fsl_spm_reslice_neg_exc.nii.gz',
-             'fsl_reslice_spm.nii.gz', 'fsl_reslice_spm_pos_exc.nii.gz','fsl_reslice_spm_neg_exc.nii.gz')
-        )
-    else:
-        resl_images = (
-            resl_images +
-            # There is only one excursion set for ds120 (no separate + and -)
-            ('afni_spm_reslice_exc.nii.gz', 'afni_reslice_spm_exc.nii.gz'))
 
     if study not in ('ds120'):
         perm_images = (
@@ -176,9 +114,9 @@ def download_data(nv_collection, study):
              ('SnPM_neg_filtered.nii.gz', 'spm_perm_exc_set_neg.nii.gz'),)
             )
 
-    # Download images (AFNI, resliced and permutation outputs)
+    # Download images (AFNI and permutation outputs)
     to_download = (
-        afni_images + tuple((u, u) for u in resl_images) + perm_images)
+        afni_images + perm_images)
 
     for image, local_name in to_download:
         url = "http://neurovault.org/media/images/" + nv_collection + '/' + image
