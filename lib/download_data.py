146e8f4fc8635bf24d0be41e13d5ca009e5b675c
cmaumet <camille.maumet@inria.fr>
2018-03-09 18:20:49 +0100

    No deactivations files for ds109 with AFNI and SPM

--- lib/download_data.py
+++ lib/download_data.py
@@ -76,30 +76,45 @@ def download_data(nv_collection, study):
         else:
             print(url + " already copied at " + local_file)
 
-    for resliced_image in (('afni_fsl_reslice.nii.gz', 'afni_fsl_reslice_pos_exc.nii.gz','afni_fsl_reslice_neg_exc.nii.gz',
-                            'afni_reslice_fsl.nii.gz', 'afni_reslice_fsl_pos_exc.nii.gz','afni_reslice_fsl_neg_exc.nii.gz',
-                            'afni_spm_reslice.nii.gz', 'afni_spm_reslice_pos_exc.nii.gz','afni_spm_reslice_neg_exc.nii.gz',
-                            'afni_reslice_spm.nii.gz', 'afni_reslice_spm_pos_exc.nii.gz','afni_reslice_spm_neg_exc.nii.gz',
-                            'fsl_spm_reslice.nii.gz', 'fsl_spm_reslice_pos_exc.nii.gz','fsl_spm_reslice_neg_exc.nii.gz',
-                            'fsl_reslice_spm.nii.gz', 'fsl_reslice_spm_pos_exc.nii.gz','fsl_reslice_spm_neg_exc.nii.gz',
-                            'afni_fsl_reslice_perm.nii.gz', 'afni_fsl_reslice_pos_exc_perm.nii.gz', 'afni_fsl_reslice_neg_exc_perm.nii.gz',
-                            'afni_reslice_fsl_perm.nii.gz', 'afni_reslice_fsl_pos_exc_perm.nii.gz', 'afni_reslice_fsl_neg_exc_perm.nii.gz',
-                            'afni_spm_reslice_perm.nii.gz', 'afni_spm_reslice_pos_exc_perm.nii.gz', 'afni_spm_reslice_neg_exc_perm.nii.gz',
-                            'afni_reslice_spm_perm.nii.gz', 'afni_reslice_spm_pos_exc_perm.nii.gz', 'afni_reslice_spm_neg_exc_perm.nii.gz',
-                            'fsl_spm_reslice_perm.nii.gz', 'fsl_spm_reslice_pos_exc_perm.nii.gz', 'fsl_spm_reslice_neg_exc_perm.nii.gz',
-                            'fsl_reslice_spm_perm.nii.gz', 'fsl_reslice_spm_pos_exc_perm.nii.gz', 'fsl_reslice_spm_neg_exc_perm.nii.gz',
-                            'afni_perm_reslice_fsl_pos_exc.nii.gz', 'afni_perm_reslice_fsl_neg_exc.nii.gz',
-                            'afni_perm_fsl_reslice_pos_exc.nii.gz', 'afni_perm_fsl_reslice_neg_exc.nii.gz',                        
-                            'afni_reslice_fsl_perm_pos_exc.nii.gz', 'afni_reslice_fsl_perm_neg_exc.nii.gz',
-                            'afni_fsl_perm_reslice_pos_exc.nii.gz', 'afni_fsl_perm_reslice_neg_exc.nii.gz',                        
-                            'afni_perm_reslice_spm_pos_exc.nii.gz', 'afni_perm_reslice_spm_neg_exc.nii.gz',
-                            'afni_perm_spm_reslice_pos_exc.nii.gz', 'afni_perm_spm_reslice_neg_exc.nii.gz',                        
-                            'afni_reslice_spm_perm_pos_exc.nii.gz', 'afni_reslice_spm_perm_neg_exc.nii.gz',
-                            'afni_spm_perm_reslice_pos_exc.nii.gz', 'afni_spm_perm_reslice_neg_exc.nii.gz',                        
-                            'fsl_perm_reslice_spm_pos_exc.nii.gz', 'fsl_perm_reslice_spm_neg_exc.nii.gz',
-                            'fsl_perm_spm_reslice_pos_exc.nii.gz', 'fsl_perm_spm_reslice_neg_exc.nii.gz',        
-                            'fsl_reslice_spm_perm_pos_exc.nii.gz', 'fsl_reslice_spm_perm_neg_exc.nii.gz',
-                            'fsl_spm_perm_reslice_pos_exc.nii.gz', 'fsl_spm_perm_reslice_neg_exc.nii.gz')):
+    resl_imgages = (
+        'afni_fsl_reslice.nii.gz', 'afni_fsl_reslice_pos_exc.nii.gz','afni_fsl_reslice_neg_exc.nii.gz',
+        'afni_reslice_fsl.nii.gz', 'afni_reslice_fsl_pos_exc.nii.gz','afni_reslice_fsl_neg_exc.nii.gz',
+        'afni_spm_reslice.nii.gz', 'afni_spm_reslice_pos_exc.nii.gz','afni_spm_reslice_neg_exc.nii.gz',
+        'afni_reslice_spm.nii.gz', 'afni_reslice_spm_pos_exc.nii.gz','afni_reslice_spm_neg_exc.nii.gz',
+        'fsl_spm_reslice.nii.gz', 'fsl_spm_reslice_pos_exc.nii.gz','fsl_spm_reslice_neg_exc.nii.gz',
+        'fsl_reslice_spm.nii.gz', 'fsl_reslice_spm_pos_exc.nii.gz','fsl_reslice_spm_neg_exc.nii.gz',
+        'afni_fsl_reslice_perm.nii.gz', 'afni_fsl_reslice_pos_exc_perm.nii.gz',
+        'afni_reslice_fsl_perm.nii.gz', 'afni_reslice_fsl_pos_exc_perm.nii.gz',
+        'afni_spm_reslice_perm.nii.gz', 'afni_spm_reslice_pos_exc_perm.nii.gz',
+        'afni_reslice_spm_perm.nii.gz', 'afni_reslice_spm_pos_exc_perm.nii.gz',
+        'fsl_spm_reslice_perm.nii.gz', 'fsl_spm_reslice_pos_exc_perm.nii.gz',
+        'fsl_reslice_spm_perm.nii.gz', 'fsl_reslice_spm_pos_exc_perm.nii.gz',
+        'afni_perm_reslice_fsl_pos_exc.nii.gz', 'afni_perm_reslice_fsl_neg_exc.nii.gz',
+        'afni_perm_fsl_reslice_pos_exc.nii.gz', 'afni_perm_fsl_reslice_neg_exc.nii.gz',                        
+        'afni_reslice_fsl_perm_pos_exc.nii.gz', 'afni_reslice_fsl_perm_neg_exc.nii.gz',
+        'afni_fsl_perm_reslice_pos_exc.nii.gz', 'afni_fsl_perm_reslice_neg_exc.nii.gz',                        
+        'afni_perm_reslice_spm_pos_exc.nii.gz', 'afni_perm_reslice_spm_neg_exc.nii.gz',
+        'afni_perm_spm_reslice_pos_exc.nii.gz', 'afni_perm_spm_reslice_neg_exc.nii.gz',                        
+        'afni_reslice_spm_perm_pos_exc.nii.gz', 'afni_reslice_spm_perm_neg_exc.nii.gz',
+        'afni_spm_perm_reslice_pos_exc.nii.gz', 'afni_spm_perm_reslice_neg_exc.nii.gz',                        
+        'fsl_perm_reslice_spm_pos_exc.nii.gz', 'fsl_perm_reslice_spm_neg_exc.nii.gz',
+        'fsl_perm_spm_reslice_pos_exc.nii.gz', 'fsl_perm_spm_reslice_neg_exc.nii.gz',        
+        'fsl_reslice_spm_perm_pos_exc.nii.gz', 'fsl_reslice_spm_perm_neg_exc.nii.gz',
+        'fsl_spm_perm_reslice_pos_exc.nii.gz', 'fsl_spm_perm_reslice_neg_exc.nii.gz'
+        )
+
+    if study not in ('ds109'):
+        # There is no deactivations in ds109 with AFNI perm and SnPM
+        resl_imgages.append(
+            'afni_fsl_reslice_neg_exc_perm.nii.gz',
+            'afni_reslice_fsl_neg_exc_perm.nii.gz',
+            'afni_spm_reslice_neg_exc_perm.nii.gz',
+            'afni_reslice_spm_neg_exc_perm.nii.gz',
+            'fsl_spm_reslice_neg_exc_perm.nii.gz',
+            'fsl_reslice_spm_neg_exc_perm.nii.gz',
+            )
+
+    for resliced_image in resl_imgages:
 
         url = "http://neurovault.org/media/images/" + nv_collection + '/' + resliced_image
         local_file = os.path.join(data_dir, resliced_image)
@@ -117,17 +132,26 @@ def download_data(nv_collection, study):
         else:
             print(url + " already downloaded at " + local_file)
 
-    for perm_image, local_name in (
-            ('perm_ttest++_Clustsim_result_t_stat_masked.nii.gz', 'afni_perm.nii.gz'), 
-            ('perm_Positive_clustered_t_stat.nii.gz', 'afni_perm_exc_set_pos.nii.gz'),
-            ('perm_Negative_clustered_t_stat.nii.gz', 'afni_perm_exc_set_neg.nii.gz'),
-            ('perm_mask.nii.gz', 'afni_perm_mask.nii.gz'),
-            ('OneSampT_tstat1.nii.gz', 'fsl_perm.nii.gz'),
-            ('05FWECorrected_OneSampT_pos_exc_set.nii.gz', 'fsl_perm_exc_set_pos.nii.gz'),
-            ('05FWECorrected_OneSampT_neg_exc_set.nii.gz', 'fsl_perm_exc_set_neg.nii.gz'),
-            ('snpmT%2B.nii.gz', 'spm_perm.nii.gz'),
-            ('SnPM_pos_filtered.nii.gz', 'spm_perm_exc_set_pos.nii.gz'),
-            ('SnPM_neg_filtered.nii.gz', 'spm_perm_exc_set_neg.nii.gz')):
+    perm_img_to_local_name = (
+        ('perm_ttest++_Clustsim_result_t_stat_masked.nii.gz', 'afni_perm.nii.gz'), 
+        ('perm_Positive_clustered_t_stat.nii.gz', 'afni_perm_exc_set_pos.nii.gz'),
+        ('perm_mask.nii.gz', 'afni_perm_mask.nii.gz'),
+        ('OneSampT_tstat1.nii.gz', 'fsl_perm.nii.gz'),
+        ('05FWECorrected_OneSampT_pos_exc_set.nii.gz', 'fsl_perm_exc_set_pos.nii.gz'),
+        ('05FWECorrected_OneSampT_neg_exc_set.nii.gz', 'fsl_perm_exc_set_neg.nii.gz'),
+        ('snpmT%2B.nii.gz', 'spm_perm.nii.gz'),
+        ('SnPM_pos_filtered.nii.gz', 'spm_perm_exc_set_pos.nii.gz')
+    )
+
+    if study not in ('ds109'):
+        # There is no deactivations in ds109 with AFNI perm
+        perm_img_to_local_name.append(
+            ('perm_Negative_clustered_t_stat.nii.gz', 'afni_perm_exc_set_neg.nii.gz'),)
+        # There is no deactivations in ds109 with SnPM (SPM perm)
+        perm_img_to_local_name.append(
+            ('SnPM_neg_filtered.nii.gz', 'spm_perm_exc_set_neg.nii.gz'),)
+
+    for perm_image, local_name in perm_img_to_local_name:
 
         url = "http://neurovault.org/media/images/" + nv_collection + '/' + perm_image
         local_file = os.path.join(data_dir, local_name)
