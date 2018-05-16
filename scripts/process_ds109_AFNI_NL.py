83a9a928d0be70fe68f7da29dc71698c927c5adc
AlexBowring <A.bowring@warwick.ac.uk>
2018-05-16 18:19:34 +0100

    scripts for AFNI reanalysis of ds109 and ds120 done

--- scripts/process_ds109_AFNI_NL.py
+++ scripts/process_ds109_AFNI_NL.py
@@ -9,13 +9,13 @@ from lib.afni_processing_NL import run_permutation_test
 from lib.afni_processing_NL import mean_mni_images
 from lib.afni_processing_NL import run_SSWarper
 
-raw_dir = '/home/maullz/NIDM-Ex/BIDS_Data/DATA/BIDS/ds001_R2.0.4'
+raw_dir = '/home/maullz/NIDM-Ex/BIDS_Data/DATA/BIDS/ds000109_R2.0.1'
 
 # Set default orientation to origin (instead of standardised space) for
 # ambiguous NIfTi (required for ds001)
 os.environ["AFNI_NIFTI_VIEW"] = "orig"
 results_dir = \
-    '/home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001'
+    '/home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds109'
 
 afni_dir = os.path.join(results_dir, 'AFNI_NL')
 
@@ -34,16 +34,20 @@ os.environ["AFNI_NIFTI_VIEW"] = "orig"
 
 # Specify the number of functional volumes ignored in the study
 TR = 2
-num_ignored_volumes = 2
+num_ignored_volumes = 0
 
 # Specify the TR that will be removed from onesets, equal to num_ignored_volumes*TR
 removed_TR_time = num_ignored_volumes*TR 
+
+# Specify the subjects of interest from the raw data
+subject_ids = [1, 2, 3, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 26, 27, 28, 30, 31, 32, 43, 48]
+subject_ids = ['{num:02d}'.format(num=x) for x in subject_ids]
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 # Copy raw anatomical and functional data to the preprocessing directory and
 # run BET on the anatomical images
-copy_raw(raw_dir, preproc_dir)
+copy_raw(raw_dir, preproc_dir, subject_ids)
 
 # Directory to store the onset files
 onset_dir = os.path.join(afni_dir, 'ONSETS')
@@ -53,28 +57,24 @@ onset_dir = os.path.join(afni_dir, 'ONSETS')
 #   {VariableLabel,{TrialType,Durations}}
 #   {{VariableLabel,VariableModLabel},{TrialType,Duration,Amplitude}}
 conditions = (
-    (('pumps_fixed', 'pumps_demean'), ('pumps_demean',)),
-    ('pumps_RT', ('pumps_demean', 'response_time')),
-    (('cash_fixed', 'cash_demean'), ('cash_demean',)),
-    ('cash_RT', ('cash_demean', 'response_time')),
-    (('explode_fixed', 'explode_demean'), ('explode_demean',)),
-    (('control_pumps_fixed', 'control_pumps_demean'),
-    ('control_pumps_demean',)),
-    ('control_pumps_RT', ('control_pumps_demean', 'response_time')))
+    ('false_belief_story', ('false belief story', 'duration')),
+    ('false_belief_question', ('false belief question', 'duration')),
+    ('false_photo_story', ('false photo story', 'duration')),
+    ('false_photo_question', ('false photo question', 'duration')))
 
 # Create onset files based on BIDS tsv files
-cond_files = create_afni_onset_files(raw_dir, onset_dir, conditions, removed_TR_time)
+cond_files = create_afni_onset_files(raw_dir, onset_dir, conditions, removed_TR_time, subject_ids)
 
 SSWarper_template = os.path.join(cwd, 'lib', 'template_AFNI_SSWarper')
-sub_level_template = os.path.join(cwd, 'lib', 'template_ds001_AFNI_level1')
-grp_level_template = os.path.join(cwd, 'lib', 'template_ds001_AFNI_level2')
-perm_template = os.path.join(cwd, 'lib', 'template_ds001_AFNI_perm_test')
+sub_level_template = os.path.join(cwd, 'lib', 'template_ds109_AFNI_level1')
+grp_level_template = os.path.join(cwd, 'lib', 'template_ds109_AFNI_level2')
+perm_template = os.path.join(cwd, 'lib', 'template_ds109_AFNI_perm_test')
 
 # Run SSWarper AFNI command on each subject to strip skull and warp to MNI template
 run_SSWarper(preproc_dir, SSWarper_template)
 
 # Run a GLM combining all the fMRI runs of each subject
-run_subject_level_analyses(preproc_dir, onset_dir, level1_dir, sub_level_template)
+#run_subject_level_analyses(preproc_dir, onset_dir, level1_dir, sub_level_template)
 
 # Run the group-level GLM
 #run_group_level_analysis(level1_dir, level2_dir, grp_level_template)
